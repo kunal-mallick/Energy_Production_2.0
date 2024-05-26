d75031738135cd24c788bed7f356d7723d6915e5
Kunal Mallick <kunalmallicks3@gmail.com>
2024-05-26 12:30:57 +0530

    Adding function

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 app=Flask(__name__)
 
 @app.route('/')
 def welcome():
     return render_template('index.html')
 
-
-
+@app.route('/pre', methods=['POST'])
+def predict():
+    temp = request.form["temp"]
+    ev = request.form["ev"]
+    ap = request.form["ap"]
+    avg = (temp+ev+ap)/2
+    return render_template('index.html', result =''.format(avg))
 
 if __name__=='__main__':
     app.run(debug=True)
