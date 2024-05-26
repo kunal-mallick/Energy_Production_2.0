381372aaa332cfc892f7f849301b40afc06f2ac7
Kunal Mallick <kunalmallicks3@gmail.com>
2024-05-26 21:21:23 +0530

    competed with flask

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask,render_template, request
 app=Flask(__name__)
+import numpy as np
 
 @app.route('/')
 def welcome():
-    return render_template('index.html')
+    return render_template('index.html', r1='Check Your', r2='Energy', r3 = ' Production')
 
-# @app.route('/pre', methods=['POST'])
-# def predict():
-#     temp = request.form["temp"]
-#     ev = request.form["ev"]
-#     ap = request.form["ap"]
-#     avg = (temp+ev+ap)/2
-#     return render_template('index.html', result =''.format(avg))
+@app.route('/pre', methods=['POST'])
+def pre():
+    temp = float(request.form["temp"])
+    ev = float(request.form["ev"])
+    ap = float(request.form["ap"])
+    avg = (temp+ev+ap)/2
+    return render_template('index.html', r1='Energy Product is', r2='{}'.format(avg), r3 = ' mw')
 
 if __name__=='__main__':
     app.run(debug=True)
